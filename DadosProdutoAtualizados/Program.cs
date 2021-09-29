cea12eec26ff0ae0238cffa5fec9623c9c0d885a
netoschmitt <agneloschmittneto@gmail.com>
2021-09-29 17:31:20 -0300

    Add project files.

---
+++ DadosProdutoAtualizados/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace DadosProdutoAtualizados {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p = new Produto();
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o número de produtos a ser adicionados ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qtd);
+            Console.WriteLine("Dados atualizados "+ p);
+
+            Console.WriteLine();
+            Console.WriteLine("Digite o numero de produtos a ser Removidos do estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qtd); Console.WriteLine("Dados Atualizados " + p);
+        }
+    }
+}
